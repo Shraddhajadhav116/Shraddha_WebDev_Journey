b951242fd8f0dfea90227ad6a58136f0b4b01631
Shraddhajadhav116 <jadhavshraddha161@gmail.com>
2025-07-17 18:40:15 +0530

    Create script.js

---
+++ Web_Dev_Journey/Javascript/Javascript_Concepts/script.js
@@ -0,0 +1,14 @@
+let color="Red";
+
+if(color=="Green"){
+    console.log("Go!");
+}
+else if(color=="Yellow"){
+    console.log("Go Slow!");
+}
+else if(color=="Red"){
+    console.log("Stop !")
+}
+else{
+    console.log("This traffic signal is mand !")
+}
